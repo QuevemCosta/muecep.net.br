58ae5d6723d636d95dd70050f7b3575e01be634c
QuevemCosta <quevemcostadacruz@gmail.com>
2020-12-11 18:19:35 -0300

    validação de formulario

--- src/pages/home/main.js
+++ src/pages/home/main.js
@@ -1,10 +1,60 @@
 const submit = document.getElementById('submit')
 
+function removerAcentos(str) {
+
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/, "");
+}
+function replaceSpaceInPlus(str){
+
+	return str.replace(/\s/g, "+")
+}
+
 function creatUrl(local){
 	const protocolo = 'https://'
 
-	console.log(local.rua)
 	return `${protocolo}viacep.com.br/ws/${local.uf}/${local.cidade}/${local.rua}/json/`;
+}
+
+function validatForm(rawData){
+
+	const {uf, cidade, rua} = rawData
+
+	const data = {
+		uf: uf.toUpperCase(),
+		cidade: replaceSpaceInPlus(removerAcentos(cidade)),
+		rua: replaceSpaceInPlus(removerAcentos(rua))
+
+	}
+	return data
+}
+
+function renderCep(dataApi){
+	const {cep} = dataApi
+
+	const spanEl = document.createElement('span');
+	const content = document.createTextNode(`Seu Cep:${cep}`);
+
+	spanEl.appendChild(content);
+
+	const renderData = document.querySelector('.result')
+	renderData.appendChild(spanEl)
+}
+
+async function send(url){
+	 await fetch(url)
+	.then(response => response.json()) 
+	.then(result => { 
+	 
+	 const data = result[0];
+
+	 renderCep(data)
+
+	})
+	.catch(err => { 
+	  
+	  console.error('Failed retrieving information', err); 
+	});
+	
 }
 
 function getDataForm(){
@@ -19,15 +69,18 @@ function getDataForm(){
 		return data.push(item);
 	})
 
-	const local = { 
+	const rawData = { 
 			uf:data[0],
 			cidade:data[1],
 			rua:data[2]
-			} 
-		
-		const url = creatUrl(local)
-		
-		console.log(url)
+		} 
+	
+
+	const url = creatUrl(validatForm(rawData))
+	
+	send(url)
+
+	
 }
 
 submit.addEventListener('click', getDataForm, false)
